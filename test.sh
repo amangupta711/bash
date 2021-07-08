7f7ba42da04146a19d6fb36a683f3a533656f876
amangupta711 <61443803+amangupta711@users.noreply.github.com>
2021-07-08 23:58:19 +0530

    final_one_userinput

--- test.sh
+++ test.sh
@@ -3,20 +3,20 @@ read input
 output=`python3 <<END
 import base64
 a=base64.b64encode(b'$input')
-#print (a)
+print (a)
 END`
-#echo $output
+echo $output
 
 
 input1=$output
 output1=`python3 <<END
 b=$input1
 c=str(b,'utf-8')
-#print (c)
+print (c)
 
 END`
 
-#echo $output1
+echo $output1
 
 #curl -s -X POST "https://cloudkms.googleapis.com/v1/projects/aaa-acg-poc/locations/us-central1/keyRings/snowkeyring/cryptoKeys/snowkmskey/cryptoKeyVersions/1:encrypt"  -d "{\"plaintext\":\"$output1\"}"  -H "Authorization:Bearer $(gcloud auth application-default print-access-token)"  -H "Content-Type:application/json"
 
